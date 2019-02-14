94ef191e67ba080a528c73c7f32e10f9dbb758de
Mark Pikulik <mpikulik@metergroup.com>
2019-02-14 14:37:34 -0800

    static file issues

--- personal_code/settings.py
+++ personal_code/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,6 +118,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # this is what it came with og
 # Static files (CSS, JavaScript, Images)
