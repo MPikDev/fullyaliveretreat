60b3a7fc1f8d3e2c03485359606cd523698fe869
Mark Pikulik <mpikulik@metergroup.com>
2019-02-13 23:27:34 -0800

    static file issues

--- personal_code/settings.py
+++ personal_code/settings.py
@@ -119,11 +119,19 @@ USE_TZ = True
 # this is what it came with og
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+#
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static/'),
+# )
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../static/'),
+]
 
+STATIC_URL  = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../usr/local/www/documents/static/')
+
+MEDIA_ROOT  = '/var/www/html/static/'
