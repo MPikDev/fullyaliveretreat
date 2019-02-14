c05999eca7dae20e6f080c0902a14fbe913172f5
Mark Pikulik <mpikulik@metergroup.com>
2019-02-13 21:02:47 -0800

    modified the static file dir

--- personal_code/settings.py
+++ personal_code/settings.py
@@ -116,12 +116,17 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+# this is what it came with og 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, '../assets/'),
-# ]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
