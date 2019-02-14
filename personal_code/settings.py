8076dbb4db58f0d0c6fc184dced56a2cf8587ddd
Mark Pikulik <mpikulik@metergroup.com>
2019-02-13 23:01:37 -0800

    tweeked the static file name

--- personal_code/settings.py
+++ personal_code/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # this is what it came with og
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
