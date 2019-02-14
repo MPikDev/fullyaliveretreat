b7b58cf98b48b810e880a336ea8dac4b353c3184
Mark Pikulik <mpikulik@metergroup.com>
2019-02-13 21:34:26 -0800

    add flavicon

--- personal_code/settings.py
+++ personal_code/settings.py
@@ -116,7 +116,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-# this is what it came with og 
+# this is what it came with og
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
