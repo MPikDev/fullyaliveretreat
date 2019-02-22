b42a01ee2f0e2e108af90c3be51c96e67aea58e4
Mark Pikulik <mpikulik@metergroup.com>
2019-02-22 13:36:01 -0800

    added another page home2 to check out the new pics also made the buttons not full screen

--- registration/views.py
+++ registration/views.py
@@ -7,6 +7,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def home2(request):
+    return render(request, 'home2.html')
+
 def register(request):
     return render(request, 'register.html')
 
