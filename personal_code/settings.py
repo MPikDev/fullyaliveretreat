b269fb8d37a6e602c087ad27b2658285a73507b6
Mark Pikulik <het7ga@gmail.com>
2019-03-02 22:38:34 -0800

    added text change to success paid, and add .. to the staticfile

--- personal_code/settings.py
+++ personal_code/settings.py
@@ -118,7 +118,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles/')
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
