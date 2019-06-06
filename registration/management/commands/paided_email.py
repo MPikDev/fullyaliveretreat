71b0ef67f0ee728ff3e97952c55c2f98cda88f6d
Mark Pikulik <het7ga@gmail.com>
2019-06-05 22:24:51 -0700

    extend the time and working on the new email with an attachment pic of the map

---
+++ registration/management/commands/paided_email.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand
+from registration.models import Camper
+from personal_code.settings import STATIC_URL, STATIC_ROOT, STATICFILES_DIRS
+import yagmail
+import time
+
+def send_email(list_campers):
+    yag = yagmail.SMTP("fullyaliveretreat@gmail.com", "REMOVED")
+    for camper in list_campers:
+        receiver_email = camper.email
+
+        body = """\
+            Hello %s,
+
+            Fully Alive Retreat is around the corner and we wanted to remind you that registration is Friday June 14th from 4 - 6 PM. The building in which registration will be held is the Welcome Center. The location is indicated on the map attached to this email. During registration you will get a map of the camp, told where your cabin will be, and you will have to sign a camp waiver form. If you are not able to register 4-6 PM, please reply to this email or contact Mark (509-979-5419) and mention when you will be arriving. Thank you.
+
+        """ % camper.first_name
+        img = yagmail.inline( STATIC_URL + 'registration/camp_map_reg.png')
+        yag.send(
+            to=receiver_email,
+            subject="Deadline Reminder - Fully Alive Retreat",
+            contents=[body, img],
+        )
+
+
+def email_paided():
+    final_paid_campers_info = Camper.objects.filter(paid=True).order_by('pk')
+
+    print 'emails going to send out:', len(final_paid_campers_info)
+    send_email(final_paid_campers_info)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        start = time.time()
+        print 'starting'
+        email_paided()
+        print 'finished ', time.time() - start
