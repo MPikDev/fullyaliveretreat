a721200794222c925fc0e17a39b4988eea589d20
Mark Pikulik <het7ga@gmail.com>
2019-06-11 16:25:09 -0700

    extending for one more person until tonight

--- registration/views.py
+++ registration/views.py
@@ -23,9 +23,9 @@ def full(request):
 def register(request):
     total_campers = PayPalIPN.objects.filter(payment_status=ST_PP_COMPLETED).count()
 
-    if total_campers > 200:
+    if total_campers > 135:
         return render(request, 'full.html')
-    if datetime.datetime.now() > datetime.datetime(2019, 6, 7, 6, 0):
+    if datetime.datetime.now() > datetime.datetime(2019, 6, 12, 7, 0):
         return render(request, 'closed.html')
 
     camper = {'total_campers': total_campers}
