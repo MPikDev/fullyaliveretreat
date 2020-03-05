66567f8272d70f2e44b1338dcc734787719368f6
MPikDev <het7ga@gmail.com>
2020-03-04 23:10:37 -0800

    modified the flip clock

--- registration/views.py
+++ registration/views.py
@@ -154,8 +154,9 @@ def pay_now(request, *args, **kwargs):
     # What you want the button to do.
     paypal_dict = {
         "business": settings.PAYPAL_RECEIVER_EMAIL,
+        # "amount": "225.00",
         "amount": "1.00",
-        "item_name": "registration for Fully Alive Retreat 2020",
+        "item_name": "Registration for Fully Alive Retreat 2020",
         'currency_code': 'USD',
         "invoice": camper_id,
         "notify_url": request.build_absolute_uri(reverse('paypal-ipn')),
