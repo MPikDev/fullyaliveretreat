a65591eab824ac1c8e9b98f24aa7911229ab9533
MPikDev <het7ga@gmail.com>
2020-07-30 00:23:48 -0700

    updated the what to expect page to the new format, got a rough draft of the desktop version. things are looking good just need to figure out the photo page

--- personal_code/urls.py
+++ personal_code/urls.py
@@ -49,5 +49,6 @@ urlpatterns = [
     url(r'^camper_info', views.camper_info, name='camper_info'),
     url(r'^2020_spring_camper_info', views.camper_info_spring_2020),
     url(r'^2019_camper_info_spring', views.camper_info_spring_2019),
+    url(r'^desktop', views.desktop),
 
 ]
