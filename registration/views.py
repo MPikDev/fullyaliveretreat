976afb186bb61baf773a01e1161afe931d59b1a7
MPikDev <het7ga@gmail.com>
2022-04-11 11:46:08 -0700

    close registration

--- registration/views.py
+++ registration/views.py
@@ -38,7 +38,7 @@ def full(request):
 
 def register(request):
     # closed
-    # return render(request, 'closed.html')
+    return render(request, 'closed.html')
 
     refund_incoices = PayPalIPN.objects.filter(payment_status=ST_PP_REFUNDED, created_at__gte=FIlTER_2022).values_list('invoice',flat=True)
     total_campers = PayPalIPN.objects.filter(payment_status=ST_PP_COMPLETED, created_at__gte=FIlTER_2022).exclude(invoice__in=refund_incoices).count()
