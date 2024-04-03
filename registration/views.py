1861bb8a19fd0625eb46ecc2a72f901b0a71cc90
MPikDev <het7ga@gmail.com>
2024-04-03 09:40:59 -0700

    revert test changes, currently paypal is down

--- registration/views.py
+++ registration/views.py
@@ -40,10 +40,10 @@ def home(request):
 def full(request):
     return render(request, 'full.html')
 
-@login_required(redirect_field_name='login')
+# @login_required(redirect_field_name='login')
 def register(request):
     # closed
-    # return render(request, 'hasnt_opened.html')
+    return render(request, 'hasnt_opened.html')
 
     refund_incoices = PayPalIPN.objects.filter(payment_status=ST_PP_REFUNDED, created_at__gte=FIlTER_2024).values_list('invoice',flat=True)
     total_campers = PayPalIPN.objects.filter(payment_status=ST_PP_COMPLETED, created_at__gte=FIlTER_2024).exclude(invoice__in=refund_incoices).count()
@@ -222,8 +222,7 @@ def pay_now(request, *args, **kwargs):
     # What you want the button to do.
     paypal_dict = {
         "business": settings.PAYPAL_RECEIVER_EMAIL,
-        # "amount":  "{price}.00".format(price=settings.CAMP_PRICE),
-        "amount":  "1.00",
+        "amount":  "{price}.00".format(price=settings.CAMP_PRICE),
         "item_name": "Registration for Fully Alive Retreat Summer 2023",
         'currency_code': 'USD',
         "invoice": camper_id,
