d90c7e912f33303cbfb0d1e7ed204b182fc59c4c
MPikDev <het7ga@gmail.com>
2025-04-16 22:59:59 -0700

    debug false

--- personal_code/settings.py
+++ personal_code/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'REMOVED'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 PAYPAL_TEST = False
 # PAYPAL_RECEIVER_EMAIL = 'events@nwasbc.com' #old one not working anymore
